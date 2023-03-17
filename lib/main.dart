c99e23f175a9dfa5f9cc656e862bde281bf20480
Yonghwi Kim <yh0921k@gmail.com>
2023-03-17 17:00:00 +0900

    feat: Stateless 위젯 구현

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          'Hello World',
-          style: TextStyle(color: Colors.white, fontSize: 20.0),
-        ),
-      ),
-    ),
-  ));
+  runApp(const MaterialApp(home: HomeScreen()));
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(child: Text('Hello world2')),
+    );
+  }
 }
