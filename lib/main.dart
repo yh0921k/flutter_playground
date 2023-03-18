20b00648139dd36b7d8cae5e9ef0d4bf06928fed
Yonghwi Kim <yh0921k@gmail.com>
2023-03-18 17:00:00 +0900

    feat: 스크린 패키지 분리

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_playground/screen/home_screen.dart';
 
 void main() {
-  runApp(
-      const MaterialApp(debugShowCheckedModeBanner: false, home: HomeScreen()));
-}
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color(0xFFF99231),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Image.asset(
-            'asset/img/logo.png',
-          ),
-          const CircularProgressIndicator(
-            color: Colors.amberAccent,
-          ),
-        ],
-      ),
-    );
-  }
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: HomeScreen(),
+  ));
 }
