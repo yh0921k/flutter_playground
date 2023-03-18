c9436d2c5cca6d51b88b8388db950192f41be955
Yonghwi Kim <yh0921k@gmail.com>
2023-03-18 17:00:00 +0900

    feat: Container 배치 및 SafeArea 위젯 사용

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -5,9 +5,36 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text('Hello'),
+    return Scaffold(
+      body: SafeArea(
+        bottom: false,
+        child: Container(
+          color: Colors.black,
+          child: Column(
+            children: [
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.red,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.orange,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.yellow,
+              ),
+              Container(
+                width: 50.0,
+                height: 50.0,
+                color: Colors.green,
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
