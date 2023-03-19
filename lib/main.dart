1b3959785080434b3f3d053fc0fadf9bfb8959ae
Yonghwi Kim <yh0921k@gmail.com>
2023-03-20 00:55:14 +0900

    feat: WebView 4.x 대응

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_playground/screen/home_screen.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     home: HomeScreen(),
