29eb4166f65493f4fe9945c839f1038128f7103b
Yonghwi Kim <yh0921k@gmail.com>
2023-03-20 00:24:23 +0900

    feat: AppBar, WebView 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,89 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        bottom: false,
-        child: Container(
-          color: Colors.black,
-          child: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.orange,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.yellow,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.green,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.orange,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Container(
-                  color: Colors.red,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.orange,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.yellow,
-                  width: 50,
-                  height: 50,
-                ),
-                Container(
-                  color: Colors.green,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  color: Colors.green,
-                  width: 50,
-                  height: 50,
-                )
-              ],
-            ),
-          ]),
-        ),
+    return Scaffold(appBar: AppBar(
+      backgroundColor: Colors.deepPurpleAccent,
+      title: Text('Code Factory'),
+      centerTitle: true,
+    ),
+      body: WebView(
+        initialUrl: 'https://blog.codefactory.ai',
+        javascriptMode: JavascriptMode.unrestricted,
       ),
     );
   }
