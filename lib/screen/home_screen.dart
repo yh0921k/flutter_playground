22392f255b39bf96a4b2222635540608522fcfaa
Yonghwi Kim <yh0921k@gmail.com>
2023-03-21 23:22:02 +0900

    feat: LifeCycle 출력 추가

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -3,18 +3,55 @@ import 'package:flutter/material.dart';
 class HomeScreen extends StatefulWidget {
   final Color color;
 
-  const HomeScreen({
+  HomeScreen({
     required this.color,
     Key? key,
-  }) : super(key: key);
+  }) : super(key: key) {
+    print("Widget Constructor 실행");
+  }
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  State<HomeScreen> createState() {
+    print("createState 실행");
+    return _HomeScreenState();
+  }
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  @override
+  void initState() {
+    print("initState 실행");
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    print("didChangeDependencies 실행");
+  }
+
+  @override
+  void deactivate() {
+    print("deactivate 실행");
+    super.deactivate();
+  }
+
+  @override
+  void dispose() {
+    print("dispose 실행");
+    super.dispose();
+  }
+
+  @override
+  void didUpdateWidget(covariant HomeScreen oldWidget) {
+    print("didUpdateWidget 실행");
+    super.didUpdateWidget(oldWidget);
+  }
+
   @override
   Widget build(BuildContext context) {
+    print("build 실행");
     return Container(
       width: 50.0,
       height: 50.0,
