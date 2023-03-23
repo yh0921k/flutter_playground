0a83157c4caab3515823923f650e7d4091341d21
Yonghwi Kim <yh0921k@gmail.com>
2023-03-23 17:00:00 +0900

    feat: Timer와 라이프사이클

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -8,15 +10,35 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  Timer? timer;
+
+  @override
+  void initState() {
+    super.initState();
+    timer = Timer.periodic(Duration(seconds: 1), (timer) {
+      print('Timer');
+    });
+  }
+
+  @override
+  void dispose() {
+    if (timer != null) {
+      timer!.cancel();
+    }
+
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
           children: [1, 2, 3, 4, 5]
-              .map((e) => Image.asset(
-                    'asset/img/image_$e.jpeg',
-                    fit: BoxFit.cover,
-                  ))
+              .map((e) =>
+              Image.asset(
+                'asset/img/image_$e.jpeg',
+                fit: BoxFit.cover,
+              ))
               .toList()),
     );
   }
